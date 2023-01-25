dac1e1403f18147cb737d3b5a32943edc8bcbf6c
Michael Aldridge <aldridge.mac@gmail.com>
2023-01-25 13:25:17 -0600

    main: Support generating the shoelaces mapping file.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"log"
 	"os"
@@ -17,6 +18,25 @@ type DHCPHost struct {
 	HWAddr   []string
 	Name     string
 	Addr     string
+}
+
+// ShoelacesHost is a host that can be mapped without user input in
+// shoelaces.
+type ShoelacesHost struct {
+	Network string     `json:"network"`
+	Script  ShoeScript `json:"script"`
+}
+
+// ShoeScript is the nested script type for shoelaces.  It is only
+// supported to specify by name.
+type ShoeScript struct {
+	Name string `json:"name"`
+}
+
+// ShoelacesNetworkMap matches the structure that shoelaces expects to
+// be able to read in.
+type ShoelacesNetworkMap struct {
+	NetworkMaps []ShoelacesHost `json:"networkMaps"`
 }
 
 func main() {
@@ -56,6 +76,7 @@ func main() {
 
 	site := os.Getenv("NETBOX_SITE")
 	tag := os.Getenv("NETBOX_TAG")
+	shoetag := os.Getenv("SHOELACES_TAG_PREFIX")
 	devices, err := nb.ListDevices(site, tag)
 	if err != nil {
 		log.Println("Error listing devices:", err)
@@ -63,6 +84,7 @@ func main() {
 	}
 
 	hosts := make(map[int64]*DHCPHost, len(devices))
+	shoenets := []ShoelacesHost{}
 	for _, dev := range devices {
 		ipaddr := strings.SplitN(dev.PrimaryIPv4.Address, "/", 2)[0]
 
@@ -91,11 +113,41 @@ func main() {
 			Addr:     ipaddr,
 			HWAddr:   hwaddrs,
 		}
+
+		// Construct the shoelaces mapping if enabled.
+		if shoetag != "" {
+			for _, tag := range dev.Tags {
+				if strings.HasPrefix(tag.Slug, shoetag) {
+					// Map this script for this host's IPs
+					shoehost := ShoelacesHost{
+						Network: ipaddr + "/32",
+						Script: ShoeScript{
+							Name: strings.TrimPrefix(tag.Slug, shoetag) + ".ipxe",
+						},
+					}
+					shoenets = append(shoenets, shoehost)
+					break
+				}
+			}
+		}
 	}
 
 	for _, host := range hosts {
 		if err := hostTmpl.Execute(os.Stdout, host); err != nil {
 			log.Println("Error executing template", err)
+		}
+	}
+
+	if os.Getenv("SHOELACES_MAPFILE") != "" {
+		bytes, err := json.Marshal(ShoelacesNetworkMap{NetworkMaps: shoenets})
+		if err != nil {
+			log.Println("Error marshalling shoelaces mappings", err)
+			os.Exit(1)
+		}
+
+		if err := os.WriteFile(os.Getenv("SHOELACES_MAPFILE"), bytes, 0644); err != nil {
+			log.Println("Error writing shoelaces mappings", err)
+			os.Exit(1)
 		}
 	}
 }
--- netbox/type.go
+++ netbox/type.go
@@ -6,6 +6,14 @@ type Address struct {
 	Address string
 }
 
+// Tag is a small associated label that is used to group machines.
+// This is used by the shoelaces mapper to determine what target
+// should be mapped for the particular machine.
+type Tag struct {
+	Name string
+	Slug string
+}
+
 // Device represents the minimum information we wish to retreive from
 // the netbox devices API as opposed to using the full fat OpenAPI
 // client.
@@ -13,6 +21,7 @@ type Device struct {
 	ID          int64
 	Name        string  `json:"name"`
 	PrimaryIPv4 Address `json:"primary_ip4"`
+	Tags        []Tag   `json:"tags"`
 }
 
 // Interface represents the minimum informatino we wish to retreive
